8902a8457507095ce3e4668658fc2036d28b6b10
SermusAnton <antonsermus@mail.ru>
2018-04-27 14:00:03 +0300

    Add EncryptionCeasar and Program

---
+++ CeasarCipher/EncryptionCeasar.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Security;
+
+namespace CeasarCipher
+{
+    /// <summary>
+    /// https://ru.wikipedia.org/wiki/%D0%A8%D0%B8%D1%84%D1%80_%D0%A6%D0%B5%D0%B7%D0%B0%D1%80%D1%8F
+    /// </summary>
+    class EncryptionCeasar
+    {
+        private string [] AlfaBets = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" };
+        private string openString;
+        private string closeString;
+        private int keyCipher;
+
+        public string OpenString {
+            get {
+                return openString;
+            }
+            set {
+                openString = value;
+                Crypt();
+            }
+        }
+        public string CloseString {
+            get {
+                return closeString;
+            }
+            set {
+                closeString = value;
+                Decrypt();
+            }
+        }
+
+        public int KeyCipher {
+            private get {
+                return keyCipher;
+            }
+            set {
+                if (value == 0) {
+                    throw new Exception (@"Шифрование отсутствует!!!");
+                } else if (value < 0) {
+                    throw new Exception(@"Неверный ключ шифрования!");
+                } else {
+                    keyCipher = value;
+                }
+            }
+        }
+
+        private void Crypt() {
+            string bufString = "";   
+                for (int i = 0; i < openString.Length; i++) {
+                    char curSimbol = openString[i];
+                    if (char.IsLetter(openString[i])) {
+                        curSimbol = char.ToUpper(openString[i]);
+                        foreach (string alfaBet in AlfaBets) {
+                            for (int j = 0; j < alfaBet.Length; j++) {
+                                if (curSimbol == alfaBet[j]) {
+                                    curSimbol = alfaBet[(j + keyCipher) % alfaBet.Length];
+                                    break;
+                                }
+                            }
+                        }
+                        curSimbol = char.IsLower(openString[i]) ? char.ToLower(curSimbol) : curSimbol;
+                    }
+                    bufString += curSimbol;
+                }            
+            openString = String.Empty;
+            closeString = bufString;
+        }
+
+        private void Decrypt() {
+            string bufString = "";
+            for (int i = 0; i < closeString.Length; i++) {
+                char curSimbol = closeString[i];
+                if (char.IsLetter(closeString[i])) {
+                    curSimbol = char.ToUpper(closeString[i]);
+                    foreach (string alfaBet in AlfaBets) {
+                        for (int j = 0; j < alfaBet.Length; j++) {
+                            if (curSimbol == alfaBet[j]) {
+                                curSimbol = alfaBet[(j - keyCipher + alfaBet.Length) % alfaBet.Length];
+                                break;
+                            }
+                        }
+                    }
+                    curSimbol = char.IsLower(closeString[i]) ? char.ToLower(curSimbol) : curSimbol;
+                }
+                bufString += curSimbol;
+            }
+            closeString = String.Empty;
+            openString = bufString;
+        }
+
+    }
+}
--- CeasarCipher/Program.cs
+++ CeasarCipher/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Security;
 
 namespace CeasarCipher
 {
+
+   
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("\t Шифр Цезаря");
+            EncryptionCeasar encrypt = new EncryptionCeasar();
+            Console.Write("Введите ключ шифрования (целое число): ");  // Может быть отриц и полож, если 0 - отсут. шифрование
+            try {
+                if (int.TryParse(Console.ReadLine(), out int keyCipher)) {
+                    encrypt.KeyCipher = keyCipher;
+                } else {
+                    throw new Exception(@"Некорректный ввод!");
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Введит выражение для шифрования:");
+            encrypt.OpenString = Console.ReadLine();
+            Console.WriteLine("Зашифрованное выражение: ");
+            Console.WriteLine(encrypt.CloseString);
+            Console.WriteLine("И снова расшифруем: ");
+            encrypt.CloseString = encrypt.CloseString;
+            Console.WriteLine(encrypt.OpenString);
+
         }
     }
 }
